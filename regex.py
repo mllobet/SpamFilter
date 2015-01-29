f196723cf92d6847d7c9436346a8a941d6b6d3b7
Miquel Llobet <mllobet.cm@gmail.com>
2015-01-28 23:50:54 -0800

    Word cleaning now wokring

---
+++ regex.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import re
+
+pattern = r"'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'"
+
+findUrl = re.compile(pattern)
